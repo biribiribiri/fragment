c92574be52bb51cc400fbf2081465de44cba7636
biribiribiri <32618928+biribiribiri@users.noreply.github.com>
2021-03-05 00:23:18 +0000

    Download csv from google, and edit iso without extraction

--- patcher/patcher.go
+++ patcher/patcher.go
@@ -3,10 +3,14 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"log"
+	"net/http"
+	"os"
 	"path/filepath"
 
+	"github.com/biribiribiri/iso9660"
 	"github.com/gocarina/gocsv"
 	"golang.org/x/text/encoding/japanese"
 )
@@ -29,21 +33,73 @@ func Fatal(err error) {
 }
 
 var (
-	translatedCsv = flag.String("translatedCsv", "", "path to translated csv")
-	inputFolder   = flag.String("inputFolder", "", "original game folder")
-	outputFolder  = flag.String("outputFolder", "", "patched output")
+	translatedCsv = flag.String("translatedCsv", "https://docs.google.com/spreadsheets/d/e/2PACX-1vTEeAsyZAWYOq3NouuFryyl-U7Tk0Xxe0rar1ZPrM_qUou170P8-BX5fS2w-9tPLnzq6VquXWj3qrpO/pub?gid=1884693932&single=true&output=csv", "path to translated csv")
+	isoPath       = flag.String("isoPath", "", "Path to original fragment ISO")
+	outputIsoPath = flag.String("outputIsoPath", "", "outputIsoPath")
 	jisEncoder    = japanese.ShiftJIS.NewEncoder()
 )
+
+func filesInIsoDirectory(isoFile *iso9660.File, base string, extentMap map[string]iso9660.Extent) {
+	children, err := isoFile.GetChildren()
+	if err != nil {
+		log.Fatalf("failed to get iso folder children: %s", err)
+	}
+	for _, child := range children {
+		if child.IsDir() {
+			filesInIsoDirectory(child, filepath.Join(base, child.Name()), extentMap)
+		} else {
+			extentMap[filepath.Join(base, child.Name())] = child.Extent()
+		}
+	}
+}
+
+func filesInIso(ra io.ReaderAt) map[string]iso9660.Extent {
+	img, err := iso9660.OpenImage(ra)
+	if err != nil {
+		log.Fatalf("failed to open iso: %s", err)
+	}
+	root, err := img.RootDir()
+	if err != nil {
+		log.Fatalf("failed to open iso root dir: %s", err)
+	}
+	m := make(map[string]iso9660.Extent)
+	filesInIsoDirectory(root, "", m)
+	return m
+}
+
+func downloadCsv() []byte {
+	resp, err := http.Get(*translatedCsv)
+	if err != nil {
+		log.Fatalf("failed to download csv from google sheets: %s", err)
+	}
+
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	return body
+}
 
 func main() {
 	flag.Parse()
 	fmt.Println("fragment patcher by plm")
 
+	f, err := os.Open(*isoPath)
+	if err != nil {
+		log.Fatalf("failed to open file: %s", err)
+	}
+	defer f.Close()
+
+	extentMap := filesInIso(f)
+	log.Printf("%s", extentMap)
+
+	newIsoData, err := ioutil.ReadAll(f)
+	if err != nil {
+		log.Fatalf("failed to read input iso: %s", err)
+	}
+
 	var gameLines []*GameLine
 	gameLinesMap := map[string][]*GameLine{}
 
-	data, err := ioutil.ReadFile(*translatedCsv)
-	Fatal(err)
+	data := downloadCsv()
 	Fatal(gocsv.UnmarshalBytes(data, &gameLines))
 
 	fmt.Printf("found %v game lines\n", len(gameLines))
@@ -63,11 +119,13 @@ func main() {
 	}
 	fmt.Print(gameLinesMap)
 	for filename, lines := range gameLinesMap {
-		path := filepath.Join(*inputFolder, filename)
-		outPath := filepath.Join(*outputFolder, filename)
-		log.Printf("processing input %v to output %v", path, outPath)
-		fileData, err := ioutil.ReadFile(path)
-		Fatal(err)
+		extent, ok := extentMap[filename]
+		if !ok {
+			log.Fatalf("could not file %q in iso extent map", filename)
+		}
+		fileData := newIsoData[extent.Start : extent.Start+extent.Length]
+
+		log.Printf("processing file %v", filename)
 		for _, line := range lines {
 			log.Print("processing line: ", line)
 			for i := 0; i < line.Length+1; i++ {
@@ -86,6 +144,6 @@ func main() {
 				}
 			}
 		}
-		Fatal(ioutil.WriteFile(outPath, fileData, 0644))
 	}
+	Fatal(ioutil.WriteFile(*outputIsoPath, newIsoData, 0644))
 }
